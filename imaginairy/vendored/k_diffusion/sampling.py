1b1aeb7d7f55c104fae3742a1cd5dcebb0e80bd9
alex <xor@alexs-MacBook-Air.local>
2022-10-07 23:21:23 +0300

    Fixed wrong argument

--- imaginairy/vendored/k_diffusion/sampling.py
+++ imaginairy/vendored/k_diffusion/sampling.py
@@ -66,6 +66,7 @@ def sample_euler(
     extra_args = {} if extra_args is None else extra_args
     if "s_churn" in extra_args:
         s_churn = extra_args["s_churn"]
+        del extra_args["s_churn"]
 
     s_in = x.new_ones([x.shape[0]])
     for i in trange(len(sigmas) - 1, disable=disable):
